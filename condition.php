76e70fb303cc2878ed18da768a17eca3c00441f4
sslmzhn <shynggyssalimzhan@gmail.com>
2024-01-19 19:11:19 +0600

    search app project

---
+++ condition.php
@@ -0,0 +1,59 @@
+<?php
+/*$time=date("H");
+    if($time>4){
+        echo "Kot";
+    } else if ($time>2){
+        echo "ok";
+    } else{
+        echo "not ok";
+}
+
+$age=20;
+if ($age==20){
+    echo "osy shaq";
+}else if ($age>20){
+    echo "bolashaq";
+}else{
+    echo "otken shaq";
+}*/
+
+# Hometask 1
+/*$a=50;
+$b=10;
+if ($a==$b){
+    echo "1";
+} else if ($a>$b){
+    echo "2";
+} else{
+    echo "3";
+}*/
+
+# Hometask 2
+$a=readline('Soz sanyn terminalga engiz:');
+switch ($a){
+    case "one":
+        echo "1";
+        break;
+    case "two":
+        echo "2";
+        break;
+    case "three":
+        echo "3";
+        break;
+}
+
+
+/*# Hometask 3
+$color= "red";
+switch($color){
+    case "red":
+        echo "STOP";
+        break;
+    case "yellow":
+        echo "Get Ready";
+        break;
+    case "green":
+        echo "Go";
+        break;
+}
+*/
